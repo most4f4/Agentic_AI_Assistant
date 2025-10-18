5d37955c368befeb295ad975deed285fb9f6997b
most4f4 <mhasanalipourshahrab@myseneca.ca>
2025-10-18 17:23:03 -0400

    exported new functions in utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -14,14 +14,25 @@ from .firestore_manager import (
         load_chat_from_cloud
     )
 
+from .voice_utils import (
+    speech_to_text_whisper,
+    text_to_speech_openai,
+    autoplay_audio,
+    get_available_voices
+)
+
 __all__ = [
-        'count_messages_by_role',
-        'get_file_extension',
-        'truncate_text',
-        'format_file_size',
-        'generate_session_title',
-        'FirestoreManager',
-        'init_firestore',
-        'save_current_chat',
-        'load_chat_from_cloud'
-    ]
+    'count_messages_by_role',
+    'get_file_extension',
+    'truncate_text',
+    'format_file_size',
+    'generate_session_title',
+    'FirestoreManager',
+    'init_firestore',
+    'save_current_chat',
+    'load_chat_from_cloud',
+    'speech_to_text_whisper',
+    'text_to_speech_openai',
+    'autoplay_audio',
+    'get_available_voices'
+]
